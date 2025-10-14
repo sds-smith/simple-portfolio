bfce742e6c70a8a1e35d92abd15a8e308c2cf88a
Shawn Smith <sds.smith24@gmail.com>
2025-10-14 19:50:07 -0400

    add server endpoints, develop boilerplate pages, develop home page, footer, and header with css

--- src/server.js
+++ src/server.js
@@ -1,7 +1,26 @@
-import express from 'express';
+const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = 80;
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', async (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'home.html'))
+})
+
+app.get('/about', async (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'about.html'))
+})
+
+app.get('/portfolio', async (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'portfolio.html'))
+})
+
+app.get('/blog', async (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'blog.html'))
+})
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`)
