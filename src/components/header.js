d22364061ffd72ef90eeed812f355008eaa0344c
Shawn Smith <sds.smith24@gmail.com>
2025-11-02 15:37:54 -0500

    develop and style responsive nav

--- src/components/header.js
+++ src/components/header.js
@@ -14,18 +14,41 @@ function Header() {
                         createElement('h3', {key: 'title'}, 'Software Engineer'),
                     ]
                 ),
+                createElement(Hamburger, {key: 'hamburger'}),
                 createElement(
                     'nav', 
                     {key: 'nav'},
                     [
-                        createElement('a', {className: 'nav-link', key: 'home', href: '/'}, 'Home'),
-                        createElement('a', {className: 'nav-link', key: 'about', href: '/about'}, 'About'),
-                        createElement('a', {className: 'nav-link', key: 'portfolio', href: '/portfolio'}, 'Portfolio'),
-                        createElement('a', {className: 'nav-link', key: 'blog', href: '/blog'}, 'Blog'),
+                        createElement('a', {className: 'nav-link', id: 'home-nav-link', key: 'home', href: '/'}, 'Home'),
+                        createElement('a', {className: 'nav-link', id: 'about-nav-link', key: 'about', href: '/about'}, 'About'),
+                        createElement('a', {className: 'nav-link', id: 'portfolio-nav-link', key: 'portfolio', href: '/portfolio'}, 'Portfolio'),
+                        createElement('a', {className: 'nav-link', id: 'blog-nav-link', key: 'blog', href: '/blog'}, 'Blog'),
                     ]
                 )
             ]
         )
+    )
+}
+
+function Hamburger() {
+    return createElement(
+        'div',
+        { id: "hamburger" },
+        [
+            createElement(
+                'label',
+                { htmlFor: 'menu', tabIndex: '0', key: 'label' },
+                createElement(
+                    'span',
+                    { className: "material-symbols-outlined" },
+                    'menu'
+                ),
+            ),
+            createElement(
+                'input',
+                { id: 'menu', type: 'checkbox', key: 'checkbox' }
+            )
+        ]
     )
 }
 
